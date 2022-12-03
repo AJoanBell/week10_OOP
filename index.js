fbf1e1f9cde973c19fe28d2ac2d66bf1a90b2100
Andrea Bell <andrea.bell28@gmail.com>
2022-12-04 09:25:14 +1100

    folder and initial code attempts

---
+++ index.js
@@ -0,0 +1,173 @@
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const generateHTML = require('./src/generateHTML');
+
+const profileCards = [];
+
+// Questions user is asked in CLI
+const welcome = () => {
+    console.log(`
+
+Welcome to the team generator! ✨
+    
+Please build your team.`);
+}
+
+const collectPeopleData = () => {
+    const addManagerData = () => {
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the team manager\'s name?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the team manager\'s id?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the team manager\'s email?',  
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'What is the team manager\'s office number?',
+            },
+        ])
+        .then((answers) => {
+            const createManager = new Manager(answers.name, parseInt(answers.id), answers.email, parseInt(answers.officeNumber));
+            const manager = {
+                name: createManager.getName(),
+                role: createManager.getRole(),
+                id: createManager.getId(),
+                email: createManager.getEmail(),
+                officeNumber: createManager.getOfficeNumber(),
+            };
+            profileCards.push(manager);
+            decideAddMember();
+        });
+    }
+
+    addManagerData();
+    
+    const addEngineerData = () => {
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the engineer\'s name?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the engineer\'s id?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the engineer\'s email?',  
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is the engineer\'s GitHub username?',  
+            }
+        ])
+        .then((answers) => {
+            const createEngineer = new Engineer(answers.name, parseInt(answers.id), answers.email, answers.github);
+            const engineer = {
+                name: createEngineer.getName(),
+                role: createEngineer.getRole(),
+                id: createEngineer.getId(),
+                email: createEngineer.getEmail(),
+                github: createEngineer.getGithub(),
+            };
+            profileCards.push(engineer);
+            decideAddMember();
+        });
+    }
+    
+    const addInternData = () => {
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the intern\'s name?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the intern\'s id?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the intern\'s email?',  
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What is the intern\'s school?',  
+            },
+        ])
+        .then((answers) => {
+            const createIntern = new Intern(answers.name, parseInt(answers.id), answers.email, answers.school);
+            const intern = {
+                name: createIntern.getName(),
+                role: createIntern.getRole(),
+                id: createIntern.getId(),
+                email: createIntern.getEmail(),
+                school: createIntern.getSchool(),
+            };
+            profileCards.push(intern);
+            decideAddMember();
+        });
+    }
+
+    const decideAddMember = () => {
+        inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'decision',
+                message: 'Which type of team member would you like to add?',
+                choices: ['Engineer','Intern','I don\'t want to add any more team members.'],
+            },
+        ])
+        .then((answers) => {        
+            if (answers.decision === 'Engineer') {
+                addEngineerData();
+            } else if (answers.decision === 'Intern') {
+                addInternData();
+            } else {
+                createHTMLFile(profileCards);
+                return
+            }
+        });
+    }
+}
+
+// Create HTML file with profiles based on user inputs
+const createHTMLFile = (profileCards) => {
+    const htmlPageContent = generateHTML(profileCards);
+    
+    fs.writeFile('./dist/index.html', htmlPageContent, (err) => err ? console.log(err) : console.log('Successfully created index.html!'))
+}
+
+// Initialise app
+const init = () => {
+    welcome();
+    collectPeopleData();
+}
+
+init();
---
+++ src/generateHTML.js
@@ -0,0 +1,73 @@
+function generateHTML(profileCards) {    
+    let totalCardHTML = '';
+    
+    const createCardHTML = (card, icon, uniqueListItem) => {
+        return `
+                <div class="card mx-2 my-3 px-0 col-12 col-sm-6 col-md-4 col-xl-3">
+                    <div class="card-header">
+                        <h2>${card.name}</h2>
+                        <h3>${card.role} ${icon}</h3>
+                    </div>
+                    <div class="card-body text-dark">
+                        <div class="card-text">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">ID: <span>${card.id}</span></li>
+                                <li class="list-group-item">Email: <a href="mailto:${card.email}" target="_blank" rel="noopener noreferrer">${card.email}</a></li>
+                                <li class="list-group-item">${uniqueListItem}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>`;
+    };
+
+    profileCards.forEach(card => {
+        let icon = '';
+        let uniqueListItem = '';
+        
+        if (card.role === 'Manager') {
+            icon = '💡';
+            uniqueListItem = `Office number: ${card.officeNumber}`;
+        } else if (card.role === 'Engineer') {
+            icon = '🛠';
+            uniqueListItem = `GitHub: <a href="https://github.com/${card.github}" target="_blank" rel="noopener noreferrer">${card.github}</a>`;
+        } else {
+            icon = '📚';
+            uniqueListItem = `School: ${card.school}`;
+        }
+
+        let cardHTML = createCardHTML(card, icon, uniqueListItem);
+        totalCardHTML = totalCardHTML.concat(cardHTML);
+    });
+        
+    return `<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <!--Bootstrap-->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+        <!--Custom styling-->
+        <link rel="stylesheet" href="./style.css"/>
+        <title>My Team</title>
+    </head>
+
+    <body>
+        <!--Header-->
+        <header class="jumbotron">
+        <h1>My Team</h1>
+        </header>
+        <!--Profile cards-->
+        <div class="container-fluid">
+            <div class="row d-flex flex-wrap justify-content-around">${totalCardHTML}
+            </div>
+        </div>
+
+    <!--Bootstrap-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+    </body>
+</html>`
+}
+
+module.exports = generateHTML;
